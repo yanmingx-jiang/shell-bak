83720c017d959a9a976646855e5ec5cc432a5f2d
yanmingx-jiang <yanmingx.jiang@intel.com>
2022-03-08 14:10:52 +0800

    del file

--- shell-bak/cp_ceph_config.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-kubectl cp $(kubectl get po -n rook-ceph |grep rook-ceph-tool |awk '{print $1}'):/etc/ceph/ceph.conf /etc/ceph/ceph.conf -n rook-ceph
-kubectl cp $(kubectl get po -n rook-ceph |grep rook-ceph-tool |awk '{print $1}'):/etc/ceph/keyring  /etc/ceph/keyring  -n rook-ceph
-
